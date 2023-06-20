1e2aee39151146001d7470133fa6a1105db00869
eras254 <eraswelaw@gmail.com>
2023-06-20 09:58:05 -0700

    alphabet 10 times commit

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,16 +6,15 @@
  */
 void print_alphabet_x10(void)
 {
-	char ch;
 	int i;
-	i=0;
-	while (i < 10)
+	int j;
+
+	for (j = 1; i <= 10; i++)
 	{
-	for (ch = 'a'; ch <= 'z'; ch++)
-	{
+		for (j = 97; j <= 122; j++)
+		{
 			_putchar(j);
 		}
 		_putchar('\n');
-		i++;
 	}
 }
